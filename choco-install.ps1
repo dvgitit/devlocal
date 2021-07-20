429cb1c0ffd888f1d76b42fb685e56627fb02597
dvgitit <58548665+dvgitit@users.noreply.github.com>
2021-07-20 12:53:11 +0200

    Create choco-install.ps1

---
+++ choco-install.ps1
@@ -0,0 +1,24 @@
+# elevated check
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$isElevated = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if(-not ($isElevated)) {
+    exit
+}
+# install
+$env:chocolateyProxyLocation = '$env:CL_PROXY_FULL_LOCAL'
+.\install.ps1
+.\ChocoInstall.ps1
+# autocomplete
+$path = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+if (-not (Test-Path $path)) {
+    echo > $path
+}
+choco install autocomplete
+choco source disable -n=chocolatey
+choco source add -n=cl-choco -s="$env:CL_REPO_ARTIFACT_CHOCO"
+# proxies
+choco config set proxy "$env:CL_PROXY_FULL_LOCAL"
+choco config set proxyBypassOnLocal true
+#
+choco list --local-only
+#
